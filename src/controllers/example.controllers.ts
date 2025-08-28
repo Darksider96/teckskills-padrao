c9c359bfd953bd259c62281e6a9bf8ec134b3311
Darksider96 <johnatanmesquita96@gmail.com>
2025-08-27 21:34:49 -0300

    criando as routas, controladores, etc...

---
+++ src/controllers/example.controllers.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+
+class ExampleController 
+{
+    async helloWorld(req: Request, res: Response)
+    {
+        return res.send('Hello Typescript')
+    }
+
+}
+
+export { ExampleController };
